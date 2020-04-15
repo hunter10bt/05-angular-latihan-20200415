a0554f12769625b1b68c83afd7e4742fc94b0a12
hunter10bt <augustberlintungka@gmail.com>
2020-04-15 10:19:55 +0700

    modified pattern making

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,6 +26,15 @@ export class AppComponent  {
     this.result = Math.pow(this.base, this.power);
     this.pattern = ""
 
+    if (this.base > this.power){
+      var max = this.base;
+      var min = this.power;
+    }
+    else{
+      var max = this.power;
+      var min = this.base;
+    }
+
     if(this.result % 2 == 0){
       this.pattern = this.pattern.concat("Pola B\n");
       var start = this.power;
@@ -39,14 +48,14 @@ export class AppComponent  {
     }
     else {
       this.pattern = this.pattern.concat("Pola A\n");
-      var start = this.power;
+      var start = max;
       var lim2 = 0;
 
       for (var line = start; line > 0; line--){
         for (var char = line; char > 0; char--){
           this.pattern = this.pattern.concat("*");
         }
-        if(lim2 < this.base){
+        if(lim2 < min){
           for (var char = 0; char <= lim2; char++){
             this.pattern = this.pattern.concat("o")
           }
