1aa7579b94491b831987b1a35b40b03f6cc0a64a
hunter10bt <augustberlintungka@gmail.com>
2020-04-15 09:58:15 +0700

    added pattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,5 +24,36 @@ export class AppComponent  {
 
   calculate(){
     this.result = Math.pow(this.base, this.power);
+    this.pattern = ""
+
+    if(this.result % 2 == 0){
+      this.pattern = this.pattern.concat("Pola B\n");
+      var start = this.power;
+
+      for (var line = start; line <= this.result; line++){
+        for (var char = 0; char < line; char++){
+          this.pattern = this.pattern.concat("*");
+        }
+        this.pattern = this.pattern.concat("A");
+      }
+    }
+    else {
+      this.pattern = this.pattern.concat("Pola A\n");
+      var start = this.power;
+      var lim2 = 0;
+
+      for (var line = start; line > 0; line--){
+        for (var char = line; char > 0; char--){
+          this.pattern = this.pattern.concat("*");
+        }
+        if(lim2 < this.base){
+          for (var char = 0; char <= lim2; char++){
+            this.pattern = this.pattern.concat("o")
+          }
+        }
+        lim2++;
+        this.pattern = this.pattern.concat("A");
+      }
+    }
   }
 }
