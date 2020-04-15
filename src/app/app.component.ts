fc9797c92c11db64d5fd520b878a6cbb93b913c1
hunter10bt <augustberlintungka@gmail.com>
2020-04-15 09:24:14 +0700

    added power operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'User';
+  base = 0;
+  power = 0;
+  result = 0;
+  pattern = "";
+
+  setbase(event:any){
+    this.base = event.target.value;
+    this.calculate();
+  }
+
+  setpower(event:any){
+    this.power = event.target.value;
+    this.calculate();
+  }
+
+  calculate(){
+    this.result = 1;
+    var iter = 0;
+
+    for (iter = 0; iter < this.power; iter++){
+      this.result = this.result * this.base;
+    }
+
+    console.log(this.result);
+  }
 }
