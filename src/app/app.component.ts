ceb9d58094598652c068524bd59aeba168f09f30
hunter10bt <augustberlintungka@gmail.com>
2020-04-15 09:40:30 +0700

    added power operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, enableProdMode } from '@angular/core';
 
 @Component({
   selector: 'my-app',
@@ -9,7 +9,7 @@ export class AppComponent  {
   name = 'User';
   base = 0;
   power = 0;
-  result = 0;
+  result = 1;
   pattern = "";
 
   setbase(event:any){
@@ -23,13 +23,6 @@ export class AppComponent  {
   }
 
   calculate(){
-    this.result = 1;
-    var iter = 0;
-
-    for (iter = 0; iter < this.power; iter++){
-      this.result = this.result * this.base;
-    }
-
-    console.log(this.result);
+    this.result = Math.pow(this.base, this.power);
   }
 }
